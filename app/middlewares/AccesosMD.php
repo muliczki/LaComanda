239665899a8a2c202f7905bc391b410c24ce1769
Your Name <you@example.com>
2021-06-14 18:09:29 -0300

    cambio clase jwt

--- app/middlewares/AccesosMD.php
+++ app/middlewares/AccesosMD.php
@@ -4,7 +4,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Psr7\Response;
 
-require_once './classes/JWT.php';
+require_once './classes/Jwt.php';
 
 class AccesosMD
 {
